a38261d3b94e4f0f3632514cfb7e943c00a7827f
ChenHao <276073970@qq.com>
2017-09-18 20:16:42 +0800

    first commit

---
+++ test01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for(i=1; i<=10; i++)
+	{
+		printf("No.%d\n", i);
+	}
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	printf("Hello World!\n");
+	return 0;
+}
