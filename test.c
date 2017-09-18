c46d841cf1c0797a4b4210591839372397ebc242
ChenHao <276073970@qq.com>
2017-09-18 21:00:04 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!");
+	return 0;
+}
+
+int a(int x, int y){
+	printf("The sum is:%d", x + y);
+}
+
+freopen("test.in", "r", stdin);
+while(scanf("%d", &m)!=EOF)
+{
+
+}
+fclose(stdin);
